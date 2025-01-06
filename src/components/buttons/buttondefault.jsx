edc11cee45e382b68f6857d37ebaa3077bc0561f
EdissonFacundo <aquinoedissonfacundo@gmail.com>
2025-01-06 02:40:31 -0300

    update button

--- src/components/buttons/buttondefault.jsx
+++ src/components/buttons/buttondefault.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
-const Button = ({ label, variant, onClick, link, type = 'button' }) => {
+const Button = ({ label, variant = 'button-default', onClick, link, type = 'button' }) => {
   const handleClick = () => {
-    if (link) {
-      window.open(link, '_blank'); // Abrir enlaces (ejemplo: WhatsApp)
-    } else if (onClick) {
-      onClick(); // Ejecutar función personalizada
+    if (onClick) {
+      onClick(); // Ejecutar función personalizada si se proporciona
     }
   };
 
-  return (
+  // Verifica si el enlace es interno (empieza con #)
+  if (link?.startsWith('#')) {
+    return (
+      <a href={link} className={`button ${variant}`}>
+        {label}
+      </a>
+    );
+  }
+
+  // Si el enlace es externo
+  return link ? (
+    <a href={link} className={`button ${variant}`} target="_blank" rel="noopener noreferrer">
+      {label}
+    </a>
+  ) : (
     <button className={`button ${variant}`} onClick={handleClick} type={type}>
       {label}
     </button>
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Button from '../buttons/buttondefault';
 
 const Navbar = () => {
   return (
     <div>
-      <nav className='hero__nav'>
-        <a href='#about'>Sobre</a>
-        <a href='#portfolio'>Portafolio</a>
-        <a href='#contact'>Contacto</a>
+      <nav className="hero__nav">
+        <Button label="Sobre" variant="button-default" link="#about" />
+        <Button label="Portafolio" variant="button-default" link="#portfolio" />
+        <Button label="Contacto" variant="button-default" link="#contact" />
       </nav>
     </div>
   );
