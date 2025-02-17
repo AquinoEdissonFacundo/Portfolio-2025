8211ce1d2709d62514e4544cf2e2041d2f509b21
EdissonFacundo <aquinoedissonfacundo@gmail.com>
2025-02-17 15:02:22 -0300

    update about

--- src/sectios/About.jsx
+++ src/sectios/About.jsx
@@ -4,26 +4,32 @@ const About = () => {
   return (
     <div className='about'>
       <div className='about-content'>
-        <h1 className='about-title'>Sobre mi &#123;</h1>
         <div className='about-container'>
-          <div className='about-image'>
-            <img src='computadora.svg' alt='' />
-          </div>
-          <div>
-            <p className='about-text'>
-              ¡Hola! Soy Facundo, tengo 22 años y me encanta programar. Hace 2
-              años descubrí mi pasión por la tecnología y desde entonces, no
-              paré de capacitarme. Me adapto fácilmente a distintos entornos y
-              tengo muchas ganas de seguir aprendiendo.
-            </p>
-            <p className='about-text'>
-              Actualmente, estoy buscando mi primera oportunidad laboral en el
-              mundo IT para seguir desarrollando mis habilidades y adquirir más
-              experiencia.
-            </p>
+          {' '}
+          <div className='about-container-text'>
+            <h1 className='about-title'>Sobre mi &#123;</h1>
+            <div>
+              <div className='about-image'>
+                <img src='computadora.svg' alt='' />
+              </div>
+              <div>
+                <p className='about-text'>
+                  ¡Hola! Soy Facundo, tengo 22 años y me encanta programar. Hace
+                  2 años descubrí mi pasión por la tecnología y desde entonces,
+                  no paré de capacitarme. Me adapto fácilmente a distintos
+                  entornos y tengo muchas ganas de seguir aprendiendo.
+                </p>{' '}
+                <br />
+                <p className='about-text'>
+                  Actualmente, estoy buscando mi primera oportunidad laboral en
+                  el mundo IT para seguir desarrollando mis habilidades y
+                  adquirir más experiencia.
+                </p>{' '}
+                <div className='about-bracket'>&#125;</div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className='about-bracket'>&#125;</div>
       </div>
     </div>
   );
